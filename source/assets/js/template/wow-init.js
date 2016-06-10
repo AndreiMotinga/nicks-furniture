e61afcdaafcfba147c5ec917ecf5826434eec649
Andrei Motinga <andrew.motinga@gmail.com>
2016-06-10 14:57:53 -0400

    fix animations

--- source/assets/js/template/wow-init.js
+++ source/assets/js/template/wow-init.js
@@ -1,3 +1,11 @@
+// animate shit
 $(function(){
   new WOW().init();
+
+  $(".scroll").click(function(event){
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000);
+  });
 });
